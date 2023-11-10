cf35debfa456675de2eabc0ab43045083b9df323
lukasschr <lukas.schroeder@ewe.net>
2023-11-10 16:34:33 +0100

    initial commit

---
+++ src/main/java/me/lukasschr/gpclaimrollback/GPClaimRollback.java
@@ -0,0 +1,17 @@
+package me.lukasschr.gpclaimrollback;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GPClaimRollback extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
