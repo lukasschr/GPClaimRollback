4f06753e5400af879fd435174cf93b815b349558
lukasschr <lukas.schroeder@ewe.net>
2023-11-10 20:35:53 +0100

    initialize event listeners & add CoreProtectAPI, func getCoreProtectAPI

--- src/main/java/me/lukasschr/gpclaimrollback/GPClaimRollback.java
+++ src/main/java/me/lukasschr/gpclaimrollback/GPClaimRollback.java
@@ -1,17 +1,34 @@
 package me.lukasschr.gpclaimrollback;
 
+import me.lukasschr.gpclaimrollback.events.ClaimDeletedEventListener;
+import net.coreprotect.CoreProtect;
+import net.coreprotect.CoreProtectAPI;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class GPClaimRollback extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        // initialize event listeners
+        getServer().getPluginManager().registerEvents(new ClaimDeletedEventListener(this), this);
     }
 
     @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    public void onDisable() {}
+
+    public CoreProtectAPI getCoreProtectAPI() {
+        Plugin plugin = getServer().getPluginManager().getPlugin("CoreProtect");
+
+        if (!(plugin instanceof CoreProtect)) { // check if CoreProtect is loaded
+            return null;
+        }
+
+        CoreProtectAPI CoreProtect = ((CoreProtect) plugin).getAPI();
+        if (!CoreProtect.isEnabled()) { // check if the API is enabled
+            return null;
+        }
+
+        return CoreProtect;
     }
 }
